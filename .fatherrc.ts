933a8c1ec129bfefc935e7744e3dca919729a3a9
王建明 <wangjianming@pm360.com>
2020-09-27 10:07:27 +0800

    chore: 框架更新

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,4 +1,37 @@
 export default {
-  esm: 'rollup',
-  cjs: 'rollup',
+  entry: 'src/index.tsx',
+  esm: {
+    type: 'babel',
+    importLibToEs: true,
+  },
+  cjs: 'babel',
+  extraBabelPlugins: [
+    [
+      'import',
+      {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: false,
+      },
+      'import-antd',
+    ],
+    [
+      'import',
+      {
+        libraryName: 'lodash',
+        libraryDirectory: '',
+        camel2DashComponentName: false,
+      },
+      'import-lodash',
+    ],
+    [
+      'import',
+      {
+        libraryName: 'ahooks',
+        libraryDirectory: 'es',
+        camel2DashComponentName: false,
+      },
+      'import-a-hooks',
+    ],
+  ],
 };
--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,47 @@
 import { defineConfig } from 'dumi';
 
+// more config: https://d.umijs.org/config
 export default defineConfig({
-  title: 'born-ui',
-  favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  title: 'Born UI',
+  favicon:
+    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  logo:
+    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
-  // more config: https://d.umijs.org/config
+  hash: true,
+  dynamicImport: {
+    loading: '@ant-design/pro-skeleton',
+  },
+  alias: {
+    '@born/born-ui': require('path').resolve(__dirname, './src'),
+  },
+  extraBabelPlugins: [
+    [
+      'import',
+      {
+        libraryName: '@born/born-ui',
+        libraryDirectory: '',
+        style: true,
+      },
+      'import-born-ui',
+    ],
+    [
+      'import',
+      {
+        libraryName: 'lodash',
+        libraryDirectory: '',
+        camel2DashComponentName: false,
+      },
+      'import-lodash',
+    ],
+    [
+      'import',
+      {
+        libraryName: 'ahooks',
+        libraryDirectory: 'es',
+        camel2DashComponentName: false,
+      },
+      'import-a-hooks',
+    ],
+  ],
 });
