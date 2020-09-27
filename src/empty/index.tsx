122e0400e4e5a7f0899bca53f21615d0cdda4eed
王建明 <wangjianming@pm360.com>
2020-09-27 10:17:30 +0800

    feat(empty): 调整默认图片

---
+++ src/empty/index.tsx
@@ -0,0 +1,19 @@
+import React, { FC, memo } from 'react';
+import { Empty as AEmpty } from 'antd';
+import { EmptyProps } from 'antd/lib/empty';
+
+export * from 'antd/lib/empty';
+
+interface EmptyType extends FC<EmptyProps> {
+  PRESENTED_IMAGE_DEFAULT: React.ReactNode;
+  PRESENTED_IMAGE_SIMPLE: React.ReactNode;
+}
+
+const Empty: EmptyType = (props) => {
+  return <AEmpty image={AEmpty.PRESENTED_IMAGE_SIMPLE} {...props} />;
+};
+
+Empty.PRESENTED_IMAGE_DEFAULT = AEmpty.PRESENTED_IMAGE_DEFAULT;
+Empty.PRESENTED_IMAGE_SIMPLE = AEmpty.PRESENTED_IMAGE_SIMPLE;
+
+export default memo(Empty);
---
+++ src/empty/style/index.tsx
@@ -0,0 +1,1 @@
+import 'and/lib/empty/style';
