54ab9a8732b0d9fa6369d49bc025e81a56d074b3
王建明 <wangjianming@pm360.com>
2020-10-28 23:08:02 +0800

    fix: copy antd脚本错误

--- scripts/copy-antd.js
+++ scripts/copy-antd.js
@@ -8,24 +8,39 @@ const fs = require('fs');
 const excludes = ['_util', 'locale', 'locale-provider', 'style'];
 
 const template = {
-  es: `export * from '{{PATH}}'`,
+  es: `import DefaultComponent from '{{PATH}}';
+export * from '{{PATH}}';
+export default DefaultComponent;
+  `,
   lib: `"use strict";
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-
-var _component = require("{{PATH}}");
-
+var _exportNames = {};
+exports.default = void 0;
+  
+var _component = _interopRequireWildcard(require("{{PATH}}"));
+  
 Object.keys(_component).forEach(function (key) {
   if (key === "default" || key === "__esModule") return;
+  if (Object.prototype.hasOwnProperty.call(_exportNames, key)) return;
   Object.defineProperty(exports, key, {
     enumerable: true,
     get: function get() {
       return _component[key];
     }
   });
-});`,
+});
+  
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+  
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+  
+var _default = _component.default;
+exports.default = _default;`,
   'es-style': `import '{{PATH}}'`,
   'lib-style': `"use strict";
 
@@ -44,7 +59,7 @@ function exists(src) {
 function genStyleFile(componentName, dest, mode) {
   fs.writeFileSync(
     path.join(dest, 'index.js'),
-    template[`${mode}-style`].replace('{{PATH}}', `antd/${mode}/${componentName}/style`),
+    template[`${mode}-style`].replace(/{{PATH}}/g, `antd/${mode}/${componentName}/style`),
     'utf-8',
   );
 }
@@ -71,7 +86,7 @@ function genFile(src, dest, mode) {
       fs.writeFileSync(
         _dest,
         template[ts ? 'es' : mode].replace(
-          '{{PATH}}',
+          /{{PATH}}/g,
           `antd/${mode}/${componentName}/${fileNameWithoutExt}`,
         ),
         'utf-8',
