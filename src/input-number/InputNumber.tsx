911e85b05665f102356fb09103976663541edc70
王建明 <wangjianming@pm360.com>
2020-12-09 17:04:22 +0800

    fix(input-number):  remove formatter

--- src/input-number/InputNumber.tsx
+++ src/input-number/InputNumber.tsx
@@ -11,7 +11,7 @@ export interface InputNumberProps extends AInputNumberProps {
 }
 
 const InputNumber = React.forwardRef<unknown, InputNumberProps>(
-  ({ groupSeparator = ',', suffix, style, disabled, ...restProps }, ref) => (
+  ({ /* groupSeparator = ',', */ suffix, style, disabled, ...restProps }, ref) => (
     <div
       className={classNames(`${CLASS_NAME_PREFIX}-input-number`, {
         [`${CLASS_NAME_PREFIX}-input-number-disabled`]: disabled,
@@ -20,17 +20,18 @@ const InputNumber = React.forwardRef<unknown, InputNumberProps>(
     >
       <AInputNumber
         ref={ref}
-        {...(groupSeparator && {
-          formatter: (value) => {
-            const [integer, decimal] = `${value}`.split('.');
-            let result = integer.replace(/\B(?=(\d{3})+(?!\d))/g, groupSeparator);
-            if (typeof decimal !== 'undefined') {
-              result += `.${decimal}`;
-            }
-            return result;
-          },
-          parser: (value) => `${value}`.replace(new RegExp(`(${groupSeparator}*)`, 'g'), ''),
-        })}
+        // bug https://github.com/ant-design/ant-design/issues/27748
+        // {...(groupSeparator && {
+        //   formatter: (value) => {
+        //     const [integer, decimal] = `${value}`.split('.');
+        //     let result = integer.replace(/\B(?=(\d{3})+(?!\d))/g, groupSeparator);
+        //     if (typeof decimal !== 'undefined') {
+        //       result += `.${decimal}`;
+        //     }
+        //     return result;
+        //   },
+        //   parser: (value) => `${value}`.replace(new RegExp(`(${groupSeparator}*)`, 'g'), ''),
+        // })}
         placeholder="请输入"
         disabled={disabled}
         {...restProps}
